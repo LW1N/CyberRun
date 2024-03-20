466f60b252e8cbc0432e567273aa4d45ca5dfb8d
Timothy Shen <59162884+timothyshen213@users.noreply.github.com>
2024-03-19 23:43:42 -0700

    endscene, quality of life, projectiles

---
+++ CyberRunGame/Assets/Scripts/Endgame.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitch : MonoBehaviour
+{
+    public int sceneBuildIndex;
+    public int money;
+    public GameObject uiObject;
+
+    void Start()
+    {
+        uiObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            int moneyAmount = MoneyManager.instance.money;
+            Debug.Log($"Current money amount: {moneyAmount}");
+            // Check if the object to move is not null
+            if (moneyAmount >= money)
+            {
+                SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+            }
+            else
+            {
+                uiObject.SetActive(true);
+                StartCoroutine("WaitForSec");
+            }
+        }
+    }
+    IEnumerator WaitForSec()
+    {
+        yield return new WaitForSeconds(5);
+        Destroy(uiObject);
+        Destroy(gameObject);
+    }
+}
